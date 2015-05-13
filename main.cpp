7c66f08e0e10b73832a5911b70c4d692f6412366
nikalexey <nikalexey@gmail.com>
2015-05-14 00:06:35 +0300

    use command line

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 #include <string>
 #include "md5.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		std::cout << "md5 <text>" << std::endl;
+		return -1;
+	}	
 
-	std::string text = "Hi There";
+	std::string text = argv[1];
 	std::string hash = get_md5(text);
 
 	std::cout << text << " " << hash << std::endl; 
